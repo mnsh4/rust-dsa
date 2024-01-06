33d800d405fb5308743f63081333b410a67d2793
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-06 16:22:50 -0300

    :rocket: added testing

--- src/array/algorithms/searching_algorithms/binary_search.rs
+++ src/array/algorithms/searching_algorithms/binary_search.rs
@@ -12,55 +12,26 @@
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
 
+struct Solution;
+
 impl Solution {
-    pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         let mut left = 0;
         let mut right = nums.len() as i32;
 
         while left < right {
             let mid = left + (right - left) / 2;
-
             if nums[mid as usize] == target {
-                return mid as i32; 
+                return mid as i32;
             } else if nums[mid as usize] < target {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-
-        -1 
+        -1
     }
-
-    // pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
-    //     let mut is_asc = true;
-    //     if nums.len() > 1 {
-    //         is_asc = nums[0] < nums[nums.len() - 1];
-    //     }
-    //     let mut left = 0;
-    //     let mut right = nums.len();
-
-    //     while left < right {
-    //         let mid = left + (right - left) / 2;
-
-    //         if is_asc {
-    //             match target.cmp(&nums[mid]) {
-    //                 Ordering::Less => right = mid,
-    //                 Ordering::Equal => return mid as i32,
-    //                 Ordering::Greater => left = mid + 1,
-    //             }
-    //         } else {
-    //             match target.cmp(&nums[mid]) {
-    //                 Ordering::Less => left = mid + 1,
-    //                 Ordering::Equal => return mid as i32,
-    //                 Ordering::Greater => right = mid,
-    //             }
-    //         }
-    //     }
-    //     -1
-    // }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -68,17 +39,17 @@ mod tests {
 
     #[test]
     fn test_binary_search_found() {
-        let arr = vec![-1, 0, 3, 5, 9, 12];
-        let target = &9;
+        let nums = vec![-1, 0, 3, 5, 9, 12];
+        let target = 9;
 
-        assert_eq!(binary_search(&arr, target), Some(4));
+        assert_eq!(Solution::search(nums, target), 4);
     }
 
     #[test]
     fn test_binary_search_not_found() {
-        let arr = vec![-1, 0, 3, 5, 9, 12];
-        let target = &2; 
+        let nums = vec![-1, 0, 3, 5, 9, 12];
+        let target = 2;
 
-        assert_eq!(binary_search(&arr, target), None);
+        assert_eq!(Solution::search(nums, target), -1);
     }
 }
