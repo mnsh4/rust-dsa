40fbfc20e39c41602af043f17aec84caabed5259
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-06 14:29:08 -0300

    :rocket: add linear search

--- src/array/algorithms/searching_algorithms/linear_search.rs
+++ src/array/algorithms/searching_algorithms/linear_search.rs
@@ -1,1 +1,34 @@
 // Los algoritmos de búsqueda están diseñados para buscar un elemento o recuperar un elemento de cualquier estructura de datos donde esté almacenado
+
+fn linear_search(item: &T, arr: &[T]) -> i32 {
+    let mut idx_pos = -1; 
+
+    for (idx, &data) in arr.iter().enumerate() {
+        if item == data {
+            idx_pos = idx as i32;
+            return idx_pos;
+        }
+    }
+    idx_pos
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_linear_search_found() {
+        let arr = vec![4, 2, 7, 1, 9, 5];
+        let target = &7;
+
+        assert_eq!(linear_search(&arr, target), Some(2));
+    }
+
+    #[test]
+    fn test_linear_search_not_found() {
+        let arr = vec!["Argentina", "Japon", "Paraguay"];
+        let target = &"Japon"; 
+
+        assert_eq!(linear_search(&arr, target), None);
+    }
+}
